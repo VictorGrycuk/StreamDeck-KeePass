e112a8c10cbcbc301657d9637a34708ce3a5ddb8
Victor_Grycuk <victor@grycuk.net>
2022-01-20 00:14:08 -0300

    More refactoring

---
+++ StreamDeck-KeePass/StreamDeck-KeePass/Actions/ActionBase.cs
@@ -0,0 +1,51 @@
+﻿using BarRaider.SdTools;
+using Newtonsoft.Json.Linq;
+using streamdeck_keepass.Domain;
+using StreamDeck_KeePass.Domain.Settings;
+using System.Threading.Tasks;
+
+namespace streamdeck_keepass.Actions
+{
+    abstract public class ActionBase : PluginBase
+    {
+        private KeePassPlugin plugin;
+
+        public ActionBase(SDConnection connection, InitialPayload payload) : base(connection, payload) { }
+
+        internal void CreatePlugin(GenerateSettings settings) => plugin = new KeePassPlugin(settings);
+
+        internal void CreatePlugin(RetrieveSettings settings) => plugin = new KeePassPlugin(settings);
+
+        public override void Dispose() => Logger.Instance.LogMessage(TracingLevel.INFO, $"Destructor called");
+
+        public void ExecuteAction(string logMessage)
+        {
+            var result = plugin.Invoke();
+
+            if (result == Result.WARNING)
+            {
+                Connection.ShowAlert().Wait();
+                Logger.Instance.LogMessage(TracingLevel.WARN, logMessage);
+            }
+            else
+            {
+                Connection.ShowOk().Wait();
+            }
+        }
+
+        public override void KeyReleased(KeyPayload payload) { }
+
+        public override void OnTick() { }
+
+        public override void ReceivedGlobalSettings(ReceivedGlobalSettingsPayload payload) { }
+
+        internal T ResolveSettings<T>(InitialPayload payload, object defaultConfig) where T : class
+        {
+            return payload.Settings == null || payload.Settings.Count == 0
+                  ? defaultConfig as T
+                  : payload.Settings.ToObject<T>();
+        }
+
+        internal Task SaveSettings(JObject settings) => Connection.SetSettingsAsync(settings);
+    }
+}
---
+++ StreamDeck-KeePass/StreamDeck-KeePass/Actions/ActionGenerate.cs
@@ -0,0 +1,28 @@
+﻿using BarRaider.SdTools;
+using Newtonsoft.Json.Linq;
+using streamdeck_keepass.Actions;
+using StreamDeck_KeePass.Domain.Settings;
+
+namespace StreamDeck_KeePass.Actions
+{
+    [PluginActionId("com.victorgrycuk.keepass.generate")]
+    public class ActionGenerate : ActionBase
+    {
+        private readonly GenerateSettings settings;
+
+        public ActionGenerate(SDConnection connection, InitialPayload payload) : base(connection, payload)
+        {
+            settings = ResolveSettings<GenerateSettings>(payload, GenerateSettings.CreateDefaultSettings());
+            CreatePlugin(settings);
+        }
+
+        public override void KeyPressed(KeyPayload payload) => ExecuteAction("No password was generated with the given configuration.");
+
+        public override void ReceivedSettings(ReceivedSettingsPayload payload)
+        {
+            Tools.AutoPopulateSettings(settings, payload.Settings);
+            SaveSettings(JObject.FromObject(settings));
+            CreatePlugin(settings);
+        }
+    }
+}
